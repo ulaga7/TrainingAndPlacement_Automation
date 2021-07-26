83e05e19c0d110f1b3f3dc0fd23c01226186e0a4
MrLoki357 <logiloga357@gmail.com>
2021-07-26 18:08:52 +0530

    html skeleton for homepage

---
+++ home.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&display=swap" rel="stylesheet">
+    <script src='main.js'></script>
+
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="text-center">
+        <img src="logos.png" class="rounded" alt="">
+      </div>
+      <div class="container place" style="text-align: center;">
+          <h3>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h3>
+      </div>
+      <div class="container place" style="text-align: center; ">
+          <h4>PALCEMENT CELL</h4>
+      </div>
+
+
+      <div class="container">
+        <p class="pad boder">
+            The Department of Computer Science and Engineering, established in 1985, aspires to produce computer professionals with sound technical skills and desires to revolutionize the IT arena. The dynamic faculty members endeavor to instill empirical knowledge to the students thereby helping them face the challenges of the current fast changing industry. The curriculum is revised every four years to be in tune with the evolving technology. The courses offered by the Department are B.Tech - Computer Science & Engineering, M.Tech - Distributed Computing Systems, M.Tech - Information Security.
+        </p>
+        <h3 class="heading">
+            MISSION
+        </h3>
+        <p class="pad boder">
+            The department aims to bring out future professionals who are equipped with knowledge of new technologies and serve well to the requirements of the computer industry. Students are trained in both the latest hardware and software paradigms. The department aims to produce proficient professionals ready to be absorbed by the industry.
+        </p>
+        <h3 class="heading">CURRICULUM</h3>
+        <p class="pad boder">
+            B.Tech. (Computer Science and Engineering)The Curriculum of B.Tech. Computer Science and Engineering is designed to provide solid foundation in the fundamentals of Computer Engineering. The syllabi is frequently updated to match current trends.Seminars, projects, guest lectures and other co-curricular activities are regularly organized in the department. The Department also offers two M.Tech. Programmes specialized in Distributed Computing System and Information Security, one Post-Graduate programme in Computer Applications (MCA).
+
+        </p>
+        <h3 class="heading">CORE</h3>
+        <ul class="pad boder">
+            <li>C & Data Structures</li>
+            <li> Digital Computer Fundamentals and Architecture</li>
+            <li>Micro processors and Applications</li>
+            <li>Operating systems</li>
+            <li>Computer Hardware and Trouble Shooting</li>
+            <li>Visual Programming</li>
+            <li> Management System</li>
+            <li>Computer Networks</li>
+            <li> Engineering and Architecture</li>
+            <li> Design</li>
+            <li>  Computer Graphics</li>
+            <li> Systems</li>
+            <li> Intelligence and Expert Systems</li>
+            <li> Programming</li>
+            <li> Computing</li>
+            <li> Systems</li>
+                
+                
+        </ul>
+
+        <h3 class="heading">ELECTIVES SUBJECTS</h3>
+
+        <ul class="pad boder">
+            <li>VLSI Design</li>
+            <li> Advanced Computer Architecture</li>
+            <li>Advanced Microprocessors</li>
+            <li>Knowledge Based Systems</li>
+            <li>Distributed DBMS</li>
+            <li>Robotics</li>
+            <li> Management System</li>
+            <li>Neural Networks and Pattern Recognition</li>
+            <li> Digital Signal Processing</li>
+            <li>Grid Computing</li>
+            <li> Software Quality Assurance and Testing</li>
+            <li> Fiber Optics and Laser Instrumentation</li>
+            <li>Environment Monitoring Instruments</li>
+            <li> Telemetry and Tele-control</li>
+                
+                
+        </ul>
+
+        <h3 class="heading">PROJECTS</h3>
+
+        <ul class="pad boder">
+            <li>Data Compression </li>
+            <li>Design Patterns</li>
+            <li>Genetic Algorithms</li>
+            <li>Knowledge Based Systems</li>
+            <li>Multimedia Database</li>
+            <li>.Net Framework</li>
+            <li>Optical Networks</li>
+            <li>Software Architecture</li>
+            <li>Wireless Networks</li>
+
+        </ul>
+    </div>
+
+    <div class="container heading">
+        <H3>TRAINING</H3>
+    </div>
+    <div class="container pad">
+
+        <h6 class="sidehead">Microprocessor Laboratory :</h6>
+        <p>Equipped with INTEL 8085 and higher versions of Microprocessor trainer kits, Hewlett Packard 80 channel logic analyzers, 100 MHz Digital Storage Oscilloscopes and PAL/PROM programmers, Micro controller development kits.     </p>
+
+    </div>
+
+    <div class="container pad">
+
+        <h6 class="sidehead">Hardware & Troubleshooting Lab :</h6>
+        <p>Equipped with P trainer kits, Printer trainer kit, LAN trainer, ABI4000 Board Master - special diagnostic computer, IC tester, In-circuit emulators, Logical Analyser, Electronic Design and Drafting tools.
+
+        </p>
+
+    </div>
+
+    <div class="container pad">
+
+        <h6 class="sidehead">Systems Laboratory : </h6>
+        <p>Equipped with two Xeon 3 GHz servers and 30 Intel Pentium IV @ 2.8GHz processor nodes running with Red Hat Linux 7.0 and Windows XP Professional.
+
+        </p>
+
+    </div>
+
+    <div class="container pad">
+
+        <h6 class="sidehead">Wireless LAN Laboratory :
+        </h6>
+        <p>Equipped with two Intel Pill @ 1 GHz servers, 35 Intel P4 @ 2.4 GHz based nodes (all multimedia enabled) with the state of the art fast wireless LAN technology.
+
+        </p>
+        
+
+    </div>
+    <div class="container pad boder">
+
+        <h6 class="sidehead">Thin Client Technology Laboratory :
+        </h6>
+
+        <p>Equipped with an imported SUN Ultra SPARC IV @ 1.5 GHz based server and 35 SUNRAY 100 appliances with 24 bit graphics nodes, installed with latest version of Solaris bundled with C, C++, HPC software, Office Automation Suite, Oracle 8i development suite, Java software and iPlanet development suite.
+
+        </p>
+
+    </div>
+
+    <div class="container">
+        <h3 class="heading">SOFTWARES</h3>
+        <ul class="pad boder">
+            <li>
+                Operating Systems - Netware 5.0, Windows XP, Red Hat Linux 7.0, Solaris 8.0 & Ubuntu Linux 8.04.
+            </li>
+
+            <li>
+                Languages - Turbo C, Turbo C++, Java,Borland C++, LISP,PROLOG.
+            </li>
+               
+            <li>
+                RDBMS - Oracle 10g.
+            </li>
+            <li>
+                4GLS- MS Visual Studio 6.0, MS SQL Server, Power Builder, MS back office server.
+
+            </li>
+        </ul>
+
+        <h3 class="heading">ACTIVITIES</h3>
+        <ul class="pad">
+            <li>Endowment lectures by Engineers and Academicians of National and International repute to augment the students’ knowledge in the high tech areas of Computer Science & Engineering and Information Technology.</li>
+            <li>Every year Tata Consultancy Services gives 2 awards to the B.Tech. (CSE) Students - Best Project Award & Best Student Award.
+            </li>
+            <li><span style="color: black; font-weight: bolder;"> Icon</span>, a national level symposium is organised every 3 years in which students from several prominent colleges across the country take part thereby exploring their technical and non-technical skills.
+            </li>
+            <li>Department magazine <span style="color: black; font-weight: bolder;">Cybrus</span>, released by the combined effort of students from all years of the Department, incorporates latest cyber world advances and several other interesting articles.
+            </li>
+        </ul>
+
+
+    </div>
+
+    
+
+
+</body>
+</html>
